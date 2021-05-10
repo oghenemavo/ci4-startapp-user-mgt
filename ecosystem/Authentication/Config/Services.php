6fdbd0985aa57fe93454cf1842858a2d61320e43
oghenemavo <princetunes@gmail.com>
2021-05-10 13:38:39 +0100

    Ecosystem\Authentication Module Config created

---
+++ ecosystem/Authentication/Config/Services.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ecosystem\Authentication\Config;
+
+use CodeIgniter\Config\BaseService;
+
+class Services extends BaseService
+{
+	// public static function signupLib($getShared = true) {
+    //     if ($getShared) {
+    //         return static::getSharedInstance('signupLib');
+    //     }
+    //     return new SignupLib();
+    // }
+}
